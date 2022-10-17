ce502565c8e260c7c411af62edd614af8f5d7a65
Ben Schoeffmann <benedikt.schoeffmann@medani.com>
2022-10-17 20:58:51 +0200

    update app

--- app.js
+++ app.js
@@ -1,33 +1,28 @@
 const express = require('express');
 const mysql = require("mysql")
+const path = require("path")
 const dotenv = require('dotenv')
-
-const app = express();
-app.set('view engine', 'hbs')
+const jwt = require("jsonwebtoken")
+const bcrypt = require("bcryptjs")
 
 dotenv.config({ path: './.env'})
 
-const path = require("path")
-const publicDir = path.join(__dirname, './public')
-
-app.use(express.static(publicDir))
-const bcrypt = require("bcryptjs")
-app.use(express.urlencoded({extended: 'false'}))
-app.use(express.json())
-
-app.post("/auth/register", (req, res) => {    
-    const { name, email, password, password_confirm } = req.body
-
-    // db.query() code goes here
-})
+const app = express();
 
 const db = mysql.createConnection({
     host: process.env.DATABASE_HOST,
     user: process.env.DATABASE_USER,
-    port: process.env.DATABASE_PORT,
     password: process.env.DATABASE_PASSWORD,
     database: process.env.DATABASE
 })
+
+const publicDir = path.join(__dirname, './public')
+
+app.use(express.static(publicDir))
+app.use(express.urlencoded({extended: 'false'}))
+app.use(express.json())
+
+app.set('view engine', 'hbs')
 
 db.connect((error) => {
     if(error) {
@@ -47,8 +42,4 @@ app.get("/register", (req, res) => {
 
 app.get("/login", (req, res) => {
     res.render("login")
-})
-
-app.listen(5000, ()=> {
-    console.log("server started on port 5000")
 })
